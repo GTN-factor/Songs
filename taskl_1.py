72c614777632bb3479309085ffaec264e83d527e
Sairo <my@mam.w>
2024-03-18 17:28:10 +0300

    Исправленна ошибка в первом задании

--- taskl_1.py
+++ taskl_1.py
@@ -12,6 +12,9 @@ with open("songs.csv", encoding = "utf-8") as file:
                 d = (datetime.date(2023,5,12) - datetime.date(int(dates[-1]), int(dates[-2]), int(dates[-3]))).days
                 streams = round(abs(d / (len(data[e][1]) + len(data[e][2]))) * 10000)
                 data[e][0] = str(streams)
+        dates = list(data[e][-1].split("."))
+        if (datetime.date(2002,1,1) - datetime.date(int(dates[-1]), int(dates[-2]), int(dates[-3]))).days >= 0:
+            print(f"{data[e][2]} - {data[e][1]} - {data[e][0]}")
 with open("songs_new.csv", "w", encoding = "utf-8", newline = "") as file:
     w = csv.writer(file, delimiter = ";", quotechar = "'")
     w.writerow(["streams","artist_name","track_name","date"])
